f19115a9dedc3541acc339d87b3f131a326b275c
KenTaghap <109163116+KenTaghap@users.noreply.github.com>
2023-10-15 12:00:14 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,28 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php'; // Autoload the MongoDB PHP extension
+
+// Replace these with your MongoDB Atlas connection details
+$mongoClient = new MongoDB\Client("mongodb+srv://kenUser:KenPassword@atlascluster.qrj9egp.mongodb.net/Agriculture?retryWrites=true&w=majority/examples");
+
+// Select the database and collection
+$database = $mongoClient->examples;
+$collection = $database->people;
+
+// Data to be inserted
+$data = [
+    'name' => 'John Doe',
+    'email' => 'johndoe@example.com',
+    'age' => 30
+];
+
+// Insert data into the collection
+$result = $collection->insertOne($data);
+
+// Check if the insertion was successful
+if ($result->getInsertedCount() > 0) {
+    echo "Data inserted successfully!";
+} else {
+    echo "Failed to insert data.";
+}
+?>
+
